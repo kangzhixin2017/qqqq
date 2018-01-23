e0114e738c73f162774a0d74086261b4cacc56a3
kangzhixin2017 <315206916@qq.com>
2018-01-23 19:46:09 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './home/home.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    NewsComponent,
+    HomeComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
